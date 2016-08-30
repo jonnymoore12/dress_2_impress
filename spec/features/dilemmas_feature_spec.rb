c3ec8552f2d9c591969306af58bafd4d4b569386
JulieLev <jkgee1@yahoo.co.uk>
2016-08-30 15:29:24 +0100

    tests added for adding pictures to dilemma

--- spec/features/dilemmas_feature_spec.rb
+++ spec/features/dilemmas_feature_spec.rb
@@ -24,9 +24,15 @@ feature 'Dilemma' do
       visit '/dilemmas'
       click_link 'Add a dilemma'
       fill_in 'Occasion', with: 'Restaurant first date'
+      attach_file 'option_1', Rails.root + 'spec/fixtures/flowers.jpg'
+      attach_file 'option_2', Rails.root + 'spec/fixtures/sumo.jpg'
       click_button 'Create Dilemma'
       expect(page).to have_content 'Restaurant first date'
+      expect(page).to have_css "img[src*='flowers.jpg']"
+      expect(page).to have_css "img[src*='sumo.jpg']"
       expect(current_path).to eq '/dilemmas'
     end
   end
+
+
 end
