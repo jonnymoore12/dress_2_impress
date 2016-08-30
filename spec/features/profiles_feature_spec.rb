dc6553c2f207701b7084114fade9601487e9b26f
Jonny Moore <jonnymoore121212@gmail.com>
2016-08-30 16:44:37 +0100

    Users sign up with name. Name appears on profile page

--- spec/features/profiles_feature_spec.rb
+++ spec/features/profiles_feature_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 feature 'User Profiles page' do
-  let!(:user){ User.create(email: 'test@gmail.com',
+  let!(:user){ User.create(name: 'mrdude',
+                          email: 'test@gmail.com',
                           password: 'password123',
                           password_confirmation: 'password123')}
 
@@ -9,6 +10,6 @@ feature 'User Profiles page' do
     sign_in
     click_link "Profile"
     expect(current_path).to eq "/users/#{user.id}"
-    expect(page).to have_content "Welcome to test@gmail.com's wardrobe"
+    expect(page).to have_content "Welcome to mrdude's wardrobe"
   end
 end
