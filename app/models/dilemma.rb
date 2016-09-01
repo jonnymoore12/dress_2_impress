fdd24bfee092410744c95ac05b32ae8be0b51fc8
acsauk <acsauk@gmail.com>
2016-09-01 19:49:08 +0100

    starts to test for validating images

--- app/models/dilemma.rb
+++ app/models/dilemma.rb
@@ -1,9 +1,13 @@
 class Dilemma < ActiveRecord::Base
-  has_attached_file :option1, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :option1, :styles => { :medium => "300x300", :thumb => "100x100" }
   validates_attachment_content_type :option1, :content_type => /\Aimage\/.*\Z/
 
-  has_attached_file :option2, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :option2, :styles => { :medium => "300x300", :thumb => "100x100" }
   validates_attachment_content_type :option2, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
+
+  validates :option1, presence: true
+  validates :option2, presence: true
+
 end
