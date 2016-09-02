69595d2738f08ddf1648fb8ffc031c6049789a13
samjbro <sjbroughton1@gmail.com>
2016-09-02 12:05:51 +0100

    adjusts ratio

--- app/models/dilemma.rb
+++ app/models/dilemma.rb
@@ -1,10 +1,10 @@
 class Dilemma < ActiveRecord::Base
   has_attached_file :option1, :styles => { :large => "600x600", :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
-  crop_attached_file :option1
+  crop_attached_file :option1, :aspect => "4:5"
   validates_attachment_content_type :option1, :content_type => /\Aimage\/.*\Z/
 
   has_attached_file :option2, :styles => { :large => "600x600", :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
-  crop_attached_file :option2
+  crop_attached_file :option2, :aspect => "4:5"
   validates_attachment_content_type :option2, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
